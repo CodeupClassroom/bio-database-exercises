d4f047ee719c96e94863be03c387460c0e908b99
Daniel Fryar <daniel.fryar@gmail.com>
2020-10-29 09:13:48 -0500

    feat:Completed walkthrough of WHERE exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -46,5 +46,19 @@ WHERE last_name LIKE 'E%'
    OR last_name LIKE '%e';
 
 # Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%e';
+#   AND last_name LIKE '%e';
+
 # Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date LIKE '%-12-25';
+
 # Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
